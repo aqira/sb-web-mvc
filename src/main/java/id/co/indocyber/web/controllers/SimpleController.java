d6181e158eedc55ceee36a30f25fc5fe12f1966e
aqira <aqira.kelana@gmail.com>
2024-04-08 13:06:16 +0700

    updated file, method, and mapping namings

--- src/main/java/id/co/indocyber/web/controllers/SimpleController.java
+++ src/main/java/id/co/indocyber/web/controllers/SimpleController.java
@@ -1,15 +1,32 @@
 package id.co.indocyber.web.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping("controller-route")
+@RequestMapping("/controller-route")
 public class SimpleController {
 
     @GetMapping("index")
-    public String index(){
+    public String index(@RequestParam(required = false) String search){
+        System.out.println("search result: " + search);
         return "controller-route/index";
+    }
+
+    @GetMapping("details")
+    public String redirectDetails(@RequestParam(required = true) Integer id){
+        return "redirect:/controller-route/details/" + id;
+    }
+
+    @GetMapping("details/{id}")
+    public String details(@PathVariable Integer id){
+        System.out.println(id);
+        return "controller-route/details";
+    }
+
+    @PostMapping("submit-form")
+    public String submitForm(){
+        System.out.println("berhasil post!");
+        return "redirect:/controller-route/index";
     }
 }
